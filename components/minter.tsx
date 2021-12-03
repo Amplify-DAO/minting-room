6cbcaf9332982a5d39a4b41e3325b92e8f5a329d
andy <coeu5a@protonmail.com>
2021-12-03 10:38:06 +0000

    fix loading states

--- components/minter.tsx
+++ components/minter.tsx
@@ -52,24 +52,28 @@ export default function Minter() {
         .then((res) => res.json())
         .then((res) => setMaticPrice(res["matic-network"].usd));
     }
-  }, [wallet?.provider, isMinting]);
+  }, [wallet?.provider, isMinting, address]);
 
   async function fetchContractValues() {
-    const web3Provider = new ethers.providers.Web3Provider(
-      (window as any).ethereum
-    );
-    const signer = web3Provider.getSigner();
-    const nftContract = new ethers.Contract(
-      NFT_CONTRACT_ADDRESS,
-      SwagAbi.abi,
-      signer
-    );
+    try {
+      const web3Provider = new ethers.providers.Web3Provider(
+        (window as any).ethereum
+      );
+      const signer = web3Provider.getSigner();
+      const nftContract = new ethers.Contract(
+        NFT_CONTRACT_ADDRESS,
+        SwagAbi.abi,
+        signer
+      );
 
-    setMaxSupply(await nftContract.functions.maxSupply());
-    setAmountSold(await nftContract.functions.totalSupply());
-    setPrice(await nftContract.functions.floorPrice());
-    setSaleIsActive(await nftContract.functions.saleIsActive());
-    setLoading(false);
+      setMaxSupply(await nftContract.functions.maxSupply());
+      setAmountSold(await nftContract.functions.totalSupply());
+      setPrice(await nftContract.functions.floorPrice());
+      setSaleIsActive(await nftContract.functions.saleIsActive());
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   async function handleMint() {
@@ -150,72 +154,69 @@ export default function Minter() {
             </Suspense>
           </Canvas>
         </div>
-        {isLoading ? null : (
-          <>
-            {!isSaleActive ? (
-              <p className="font-bold text-xl">Coming soon...</p>
-            ) : (
-              <div>
-                {address && (
-                  <div className="space-y-2">
-                    <p className="font-bold text-xl">Chapel Genensis</p>
-                    <div className="grid grid-cols-2 items-center text-sm leading-6 font-medium space-y-1">
-                      <p>Remaining Supply</p>
-                      <p>
-                        {maxSupply.toString() - amountSold.toString()}/
-                        {maxSupply.toString()}
-                      </p>
-                      <p>Price</p>
-                      <p>
-                        {totalPrice} MATIC ($
-                        {(maticPrice * totalPrice).toFixed(2)})
-                      </p>
-                    </div>
-                  </div>
-                )}
-                {receipt ? (
-                  <Button disabled={isMinting}>
-                    <a
-                      href={`https://mumbai.polygonscan.com/tx/${receipt.transactionHash}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      View transaction
-                    </a>
-                  </Button>
-                ) : address ? (
-                  <>
-                    <div>
-                      <label
-                        htmlFor="quantity"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Quantity
-                      </label>
-                      <select
-                        onChange={(e) => setQuantity(e.target.value)}
-                        id="quantity"
-                        name="quantity"
-                        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                      >
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                      </select>
-                    </div>
-                    <Button onClick={handleMint} disabled={isMinting}>
-                      {isMinting ? "Minting NFT..." : "Mint"}
-                    </Button>
-                  </>
-                ) : (
-                  <div className="flex justify-center">
-                    <Button onClick={handleOnboard}>Connect wallet</Button>
-                  </div>
-                )}
+
+        {!isSaleActive ? (
+          <p className="font-bold text-xl">Coming soon...</p>
+        ) : (
+          <div>
+            {address && (
+              <div className="space-y-2">
+                <p className="font-bold text-xl">Chapel Genensis</p>
+                <div className="grid grid-cols-2 items-center text-sm leading-6 font-medium space-y-1">
+                  <p>Remaining Supply</p>
+                  <p>
+                    {maxSupply.toString() - amountSold.toString()}/
+                    {maxSupply.toString()}
+                  </p>
+                  <p>Price</p>
+                  <p>
+                    {totalPrice} MATIC ($
+                    {(maticPrice * totalPrice).toFixed(2)})
+                  </p>
+                </div>
               </div>
             )}
-          </>
+            {receipt ? (
+              <Button disabled={isMinting}>
+                <a
+                  href={`https://mumbai.polygonscan.com/tx/${receipt.transactionHash}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  View transaction
+                </a>
+              </Button>
+            ) : address ? (
+              <>
+                <div>
+                  <label
+                    htmlFor="quantity"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Quantity
+                  </label>
+                  <select
+                    onChange={(e) => setQuantity(e.target.value)}
+                    id="quantity"
+                    name="quantity"
+                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                  >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                  </select>
+                </div>
+                <Button onClick={handleMint} disabled={isMinting}>
+                  {isMinting ? "Minting NFT..." : "Mint"}
+                </Button>
+              </>
+            ) : (
+              <div className="flex justify-center">
+                <Button onClick={handleOnboard}>Connect wallet</Button>
+              </div>
+            )}
+          </div>
         )}
       </div>
 
