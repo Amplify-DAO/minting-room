c281889218e5396d366f8f2e18016eccb8d444d9
andy <coeu5a@protonmail.com>
2021-12-04 23:30:14 +0000

    favicon, title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,8 +17,27 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Chapel Genesis</title>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
             rel="stylesheet"
